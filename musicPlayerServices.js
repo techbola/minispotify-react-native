cd29c3ce738db90c4fe987eba762f58debd5b9e2
Adebola <adebola@poptin.com>
2024-06-13 18:20:16 +0100

    setup player services - play, pause, next, previous

--- musicPlayerServices.js
+++ musicPlayerServices.js
@@ -1,8 +1,36 @@
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {RepeatMode} from 'react-native-track-player';
 
 import {playListData} from './src/constants';
 
-export async function playbackService() {
-  await TrackPlayer.setupPlayer();
+export async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getCurrentTrack();
+    isSetup = true;
+  } catch (e) {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}
+
+export async function addTrack() {
   await TrackPlayer.add(playListData);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
+
+export async function playbackService() {
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
 }
